6318f959ba1503d111dc4d58dde73d009953cb16
Danish Ansari <aaansaridan@gmail.com>
2021-08-10 14:07:55 +0530

    Added header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -7,7 +8,13 @@ export default function Home() {
         <title>Facebook</title>
       </Head>
       
-      <h1>Let's build facebook</h1>
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
 
     </div>
   )
