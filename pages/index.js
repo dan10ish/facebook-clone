36285f0c759229135b68e497aa9a00f4e42bb4ff
Danish Ansari <aaansaridan@gmail.com>
2021-08-10 20:27:45 +0530

    Added Auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getSession, session } from 'next-auth/client';
 import Head from 'next/head'
 import Header from '../components/Header'
 
 export default function Home() {
+
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
@@ -17,5 +21,16 @@ export default function Home() {
       </main>
 
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+
+  return{
+    props:{
+      session
+    }
+  }
 }
