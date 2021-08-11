23da6bd96e48ec15b1bb0a816cb6dc43cba3987b
Danish Ansari <aaansaridan@gmail.com>
2021-08-11 12:50:03 +0530

    Added Stories

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function StoryCard({ name, src, profile }) {
     return (
-        <div className="relative h-14 w-14">
+        <div className="relative h-14 w-14 md:h-20 md:w-20 lg:h-56 lg:w-32 cursor-pointer overflow-x p-3">
             <Image
                 className="absolute opacity-0 lg:opacity-100 rounded-full z-50 top-10"
                 src={profile}
